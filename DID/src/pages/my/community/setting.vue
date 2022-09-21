e42a04bbb21c37c12b8e358ee423b1b5c23e39d4
吴之乐 <2292777095@qq.com>
2022-09-21 09:26:37 +0800

    '代码备份'

--- DID/src/pages/my/community/setting.vue
+++ DID/src/pages/my/community/setting.vue
@@ -1,22 +1,59 @@
 <template>
-  <van-pull-refresh v-model="loading" @refresh="onRefresh">
-    <div class="community_wrap fullscreen">
-      <page-header title="社区信息" theme="dark" />
-      <div class="content">
-        <h3 class="title">粒紫社区</h3>
-      </div>
+  <div class="community_wrap fullscreen bg-gray">
+    <page-header title="社区信息" theme="dark" />
+    <div class="content">
+      <van-cell class="title">粒紫社区</van-cell>
       <van-form>
-        <van-row class="row">
-          <van-col class="label" span="24">展示图片<span style="color: #999;">(不传则显示默认图片)</span></van-col>
-          <van-col span="24">
-            <div class="upload_wrap1">
-              
+        <van-cell>
+          <template slot="title">
+            <div class="label">展示图片<span style="color: #999;">(不传则显示默认图片)</span></div>
+          </template>
+          <template slot="label">
+            <div class="upload_wrap">
+              <van-uploader
+              :after-read="afterRead"
+              />
+              <div class="tip">
+                <i class="icon icon-photo"></i>
+                <p class="text">点击上传展示图片</p>
+              </div>
             </div>
-          </van-col>
-        </van-row>
+          </template>
+        </van-cell>
+        <van-field
+          v-model="form.data.desc"
+          show-word-limit
+          rows="3"
+          label="简介"
+          type="textarea"
+          maxlength="100"
+          placeholder="请输入…"
+        />
+        <van-field
+          v-model="form.data.dbq"
+          required
+          name="dbq"
+          label="电报群"
+          placeholder="填写电报群地址"
+        />
+        <van-field
+          v-model="form.data.qq"
+          name="qq"
+          label="QQ群"
+          placeholder="填写QQ群号"
+        />
+        <van-field
+          v-model="form.data.discord"
+          name="discord"
+          label="Discord"
+          placeholder="填写Discord账号"
+        />
+        <div class="btn">
+          <van-button round block type="info" native-type="submit" :disabled="submitDisable" color="#1B2945">提交</van-button>
+        </div>
       </van-form>
     </div>
-  </van-pull-refresh>
+  </div>
 </template>
 
 <script>
@@ -24,19 +61,28 @@ import pageHeader from "@/components/topBar/pageHeader.vue"
 
 export default {
   components: { pageHeader },
-  name: "community",
+  name: "communitySetting",
   data() {
     return {
       loading: false,
       status: 0,
+      form: {
+        data: {
+          desc: "",
+          dbq: "",
+          qq: "",
+          discord: ""
+        }
+      }
+    }
+  },
+  computed: {
+    submitDisable() {
+      return !!Object.values(this.form.data).includes("")
     }
   },
   methods: {
-    onRefresh() {},
-    getList() {},
-    handleLoad() {
-
-    },
+    afterRead() {},
   }
 }
 </script>
@@ -45,60 +91,42 @@ export default {
 .community_wrap {
   position: relative;
   .content {
-    padding: 0 32px;
+    position: relative;
+    flex: 1;
+    margin-top: 20px;
+    background-color: #FFF;
     .title {
-      padding: 30px 0;
-      font-size: 36px;
       color: #333;
-      font-weight: bold;
-      border-bottom: 1px solid #EEE;
+      font-size: 32px;
     }
-    .img {
-      margin: 20px 0;
-      display: block;
-      width: 100%;
-    }
-    .row {
-      padding: 30px 0;
-      .label {
-        color: #333;
-        font-size: 32px;
-      }
-      .value {
-        color: #666;
-        font-size: 32px;
+    .upload_wrap {
+      position: relative;
+      margin-top: 20px;
+      background-color: #F3F4F5;
+      .tip {
+        @include centered();
+        color: #999;
+        text-align: center;
         .icon {
-          color: #999;
-          font-size: 24px;
-          margin-left: 15px;
+          font-size: 40px;
         }
-        &.desc {
-          line-height: 1.5;
-          margin-top: 30px;
+        .text {
+          font-size: 28px;
         }
-        &.link {
-          color: #237FF8;
+      }
+      &::v-deep(.van-uploader) {
+        display: block;
+        width: 100%;
+        opacity: 0;
+        .van-uploader__upload {
+          width: 100%;
+          margin: 0;
+          height: 130px;
         }
       }
     }
     .btn {
       @include posi($b: 30px, $l: 30px, $r: 30px);
-      line-height: 96px;
-      text-align: center;
-      background-color: #247FF6;
-      font-size: 32px;
-      color: #FFF;
-      border-radius: 48px;
-      &.pending {
-        color: #237FF8;
-        background-color: #D3E5FD;
-        border: 1px solid #247FF6;
-      }
-      &.reject {
-        color: #FF6343;
-        background-color: #FFDFD8;
-        border: 1px solid #FF6343;
-      }
     }
   }
 }
