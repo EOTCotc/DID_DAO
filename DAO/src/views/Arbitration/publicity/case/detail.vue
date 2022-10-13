6352916229270875402996d675006b29e200c562
huayiya <2356535286@qq.com>
2022-10-13 15:08:16 +0800

    修改

--- DAO/src/views/Arbitration/publicity/case/detail.vue
+++ DAO/src/views/Arbitration/publicity/case/detail.vue
@@ -4,23 +4,19 @@
     <div class="content">
       <!-- 原被告信息 -->
       <van-row>
-        <van-col
-          class="lf"
-          :span="12"
-          @click="
+        <van-col class="lf"
+                 :span="12"
+                 @click="
             go('/user/arbitration/case/personnelInfo', {
               id: info.plaintiffId,
               type: 1,
             })
-          "
-        >
+          ">
           <div class="identity_wrap">
-            <img
-              v-if="info.status === 2"
-              src="../../../../assets/imgs/huangguan.png"
-              alt=""
-              class="img"
-            />
+            <img v-if="info.status === 2"
+                 src="../../../../assets/imgs/huangguan.png"
+                 alt=""
+                 class="img" />
             {{ $t("publicity.plaintiff") }}
           </div>
           <div class="user">
@@ -31,23 +27,19 @@
             {{ info.plaintiffNum }}{{ $t("publicity.ticket") }}
           </div>
         </van-col>
-        <van-col
-          class="rt"
-          :span="12"
-          @click="
+        <van-col class="rt"
+                 :span="12"
+                 @click="
             go('/user/arbitration/case/personnelInfo', {
               id: info.defendantId,
               type: 2,
             })
-          "
-        >
+          ">
           <div class="identity_wrap">
-            <img
-              v-if="info.status === 3"
-              src="../../../../assets/imgs/huangguan.png"
-              alt=""
-              class="img"
-            />
+            <img v-if="info.status === 3"
+                 src="../../../../assets/imgs/huangguan.png"
+                 alt=""
+                 class="img" />
             {{ $t("publicity.defendant") }}
           </div>
           <div class="user">
@@ -60,13 +52,11 @@
         </van-col>
       </van-row>
       <div class="process_wrap">
-        <van-progress
-          stroke-width="12"
-          :percentage="info.plaintiffNum / info.total * 100 || 0"
-          :show-pivot="false"
-          color="#4EA0F5"
-          track-color="#EC6F66"
-        />
+        <van-progress stroke-width="12"
+                      :percentage="info.plaintiffNum / info.total * 100 || 0"
+                      :show-pivot="false"
+                      color="#4EA0F5"
+                      track-color="#EC6F66" />
       </div>
       <div class="remark">
         {{ $t("publicity.launch")
@@ -90,31 +80,25 @@
       </div>
       <div class="row">
         <div class="title">{{ $t("publicity.judgment") }}</div>
-        <ul class="list" v-if="!!info.votes">
-          <li class="item" v-for="item in info.votes" :key="item.number">
+        <ul class="list"
+            v-if="!!info.votes">
+          <li class="item"
+              v-for="item in info.votes"
+              :key="item.number">
             <span class="name">{{ item.name }}</span>
             <span class="id">{{ item.number }}</span>
-            <span
-              class="identity icon-court"
-              style="color: #999"
-              v-if="item.voteStatus === 0"
-            >
-              {{ $t("publicity.no_vote") }}</span
-            >
-            <span
-              class="identity icon-court"
-              style="color: #4ea0f5"
-              v-else-if="item.voteStatus === 1"
-            >
-              {{ $t("publicity.plaintiff") }}</span
-            >
-            <span
-              class="identity icon-court"
-              style="color: #ed7269"
-              v-else-if="item.voteStatus === 2"
-            >
-              {{ $t("publicity.defendant") }}</span
-            >
+            <span class="identity icon-court"
+                  style="color: #999"
+                  v-if="item.voteStatus === 0">
+              {{ $t("publicity.no_vote") }}</span>
+            <span class="identity icon-court"
+                  style="color: #4ea0f5"
+                  v-else-if="item.voteStatus === 1">
+              {{ $t("publicity.plaintiff") }}</span>
+            <span class="identity icon-court"
+                  style="color: #ed7269"
+                  v-else-if="item.voteStatus === 2">
+              {{ $t("publicity.defendant") }}</span>
           </li>
         </ul>
       </div>
@@ -123,62 +107,62 @@
 </template>
 
 <script>
-import pageHeader from "@/components/topBar/pageHeader";
-import { caseDetail } from "@/api/publicity";
-import { transformUTCDate, getArbitrateInType } from "@/utils/utils";
+import pageHeader from '@/components/topBar/pageHeader'
+import { caseDetail } from '@/api/publicity'
+import { transformUTCDate, getArbitrateInType } from '@/utils/utils'
 
 export default {
-  name: "arbitrationCaseDetail",
+  name: 'arbitrationCaseDetail',
   components: {
     pageHeader,
   },
   data() {
     return {
       info: {},
-    };
+    }
   },
   methods: {
     getArbitrateInType,
     transformUTCDate,
     // 跳转页面
     go(path, query) {
-      this.$router.push({ path, query });
+      this.$router.push({ path, query })
     },
     getDetail() {
       const loading = this.$toast.loading({
         forbidClick: true,
-        message: this.$t("publicity.message"),
-      });
+        message: this.$t('publicity.message'),
+      })
       caseDetail(this.$route.query.arbitrateInfoId)
         .then((res) => {
-          const { code, items } = res.data;
+          const { code, items } = res.data
           if (code) {
             this.$toast.fail({
               forbidClick: true,
-              message: this.$t("publicity.message_fild"),
-            });
+              message: this.$t('publicity.message_fild'),
+            })
           } else {
-            items.total = items.plaintiffNum + items.defendantNum;
-            this.info = items;
+            items.total = items.plaintiffNum + items.defendantNum
+            this.info = items
           }
         })
         .catch(() => {
           this.$toast.fail({
             forbidClick: true,
-            message: this.$t("publicity.message_fild"),
-          });
+            message: this.$t('publicity.message_fild'),
+          })
         })
         .finally(() => {
-          loading.clear();
-        });
+          loading.clear()
+        })
     },
     // 翻页
     onLoad() {},
   },
   created() {
-    this.getDetail();
+    this.getDetail()
   },
-};
+}
 </script>
 
 <style scoped lang="scss">
@@ -253,6 +237,10 @@ export default {
     .process_wrap {
       margin-top: 20px;
     }
+    .remark {
+      margin-top: 25px;
+      line-height: 60px;
+    }
     .row {
       margin-top: 30px;
       .title {
@@ -266,6 +254,7 @@ export default {
           margin: 0;
           color: #333;
           font-size: 28px;
+          line-height: 42px;
         }
       }
     }
